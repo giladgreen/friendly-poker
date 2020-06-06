6a01cf0d276177e35a6bb6de6631f64928b38555
giladgreen <green.gilad@gmail.com>
2020-06-06 14:25:58 +0300

    a

--- front/src/App.js
+++ front/src/App.js
@@ -759,6 +759,21 @@ class App extends Component {
             <audio id='card-place-audio45' src="./card-place.mp3" preload="auto" controls="none" style={{display:'none'}}/>
             <audio id='chips-audio' src="./chips.mp3" preload="auto" controls="none" style={{display:'none'}}/>
             <div id="connection-status" className={this.state.connected ? 'connected-to-server':'connecting-to-server'}> { this.state.connected ? 'Connected' : 'Connecting..'}</div>
+            <div id="bottom-line">
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+                <div>10</div>
+                <div>11</div>
+                <div>12</div>
+                <div>13</div>
+            </div>
         </div>
     };
 
